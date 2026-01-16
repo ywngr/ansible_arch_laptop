27e492e99e42b058ee5d4a3c75773ba005a88683
ywngr <ywngrr@gmail.com>
2026-01-16 17:20:31 +0400

    up

---
+++ roles/firefox/files/user.js
@@ -0,0 +1,10 @@
+/* 
+
+All params with description https://arkenfox.github.io/gui/
+
+*/
+
+
+user_pref("places.history.enabled", false);
+user_pref("browser.sessionstore.max_tabs_undo", 0);
+user_pref("findbar.highlightAll", true);
