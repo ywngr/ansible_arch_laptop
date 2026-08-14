977da2e998c0e7bceabe904ac17e852bd221303f
ywngr <ywngrr@gmail.com>
2026-08-14 11:44:27 +0400

    up

--- roles/base/templates/scripts/brightness_timer.sh
+++ roles/base/templates/scripts/brightness_timer.sh
@@ -3,9 +3,9 @@
 HOUR=$(date +%H)
 
 if [ "$HOUR" -ge 5 ] && [ "$HOUR" -lt 21 ]; then
-	brightnessctl s 65
+	brightnessctl s 45
 	brightnessctl -d 'asus::kbd_backlight' s 0
 else
-	brightnessctl s 50
+	brightnessctl s 30
 #	brightnessctl -d 'asus::kbd_backlight' s 1
 fi
